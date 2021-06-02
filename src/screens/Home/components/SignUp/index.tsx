9f905b101f4110746bf063e01d8deb00fd52c444
Mariana Calmon <mariana.calmon@wolox.com.ar>
2021-06-02 13:29:21 -0300

    React-hook-form is added.

--- src/screens/Home/components/SignUp/index.tsx
+++ src/screens/Home/components/SignUp/index.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { useForm } from 'react-hook-form';
+
 import WoloxLogo from '../../assets/LogoWolox-Original.png';
 
 import styles from './styles.module.scss';
@@ -16,28 +18,31 @@ type InputsValues = {
 };
 
 function SignUp() {
+  const { register, handleSubmit } = useForm<InputsValues>();
+  const onSubmit = (data: InputsValues) => console.log(data);
+
   return (
-    <form className={styles.signUpForm}>
+    <form className={styles.signUpForm} onSubmit={handleSubmit(onSubmit)}>
       <img src={WoloxLogo} className={`m-bottom-3 ${styles.logo}`} />
       <div className={`column start m-bottom-3 ${styles.inputContainer}`}>
         <label className="m-bottom-2">Nombre</label>
-        <input className={styles.input} />
+        <input {...register('user.firstName')} className={styles.input} />
       </div>
       <div className={`column start m-bottom-3 ${styles.inputContainer}`}>
         <label className="m-bottom-2">Apellido</label>
-        <input className={styles.input} />
+        <input {...register('user.lastName')} className={styles.input} />
       </div>
       <div className={`column start m-bottom-3 ${styles.inputContainer}`}>
         <label className="m-bottom-2">Email</label>
-        <input type="email" className={styles.input} />
+        <input type="email" {...register('user.email')} className={styles.input} />
       </div>
       <div className={`column start m-bottom-3 ${styles.inputContainer}`}>
         <label className="m-bottom-2">Password</label>
-        <input type="password" className={styles.input} />
+        <input type="password" {...register('user.password')} className={styles.input} />
       </div>
       <div className={`column start m-bottom-5 ${styles.inputContainer}`}>
         <label className="m-bottom-2">Confirmación de Password</label>
-        <input type="password" className={styles.input} />
+        <input type="password" {...register('user.passwordConfirmation')} className={styles.input} />
       </div>
       <button type="submit" className={`m-bottom-3 ${styles.signUpButton}`} />
       <div className={`m-bottom-3 ${styles.line}`} />
