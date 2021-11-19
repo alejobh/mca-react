c4e340924340732516abce6d2f1897cdad73e0ed
Mariana Calmon <mariana.calmon@wolox.com.ar>
2021-11-19 12:51:21 -0300

    Header and BookList component tests are updated, Home test is created.

--- src/components/Header/index.test.tsx
+++ src/components/Header/index.test.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
+import { Router, Route } from 'react-router-dom';
+import i18next from 'i18next';
 
-import Header from '.';
+import Header from './index';
 
 describe('Header component', () => {
   test('render Header', () => {
     const component = render(<Header />);
-    component.debug();
+    component.getByText(`${i18next.t('Header:logout')}`);
+  });
+  test('matches last snapshot', () => {
+    const history = createMemoryHistory();
+    history.push('/login');
+    const component = render(
+      <Router history={history}>
+        <Route path="/login">
+          <Header />
+        </Route>
+      </Router>
+    );
+    expect(component).toMatchSnapshot();
+    userEvent.click(screen.getByRole('button', { name: `${i18next.t('Header:logout')}` }));
   });
 });
--- src/screens/Home/index.test.tsx
+++ src/screens/Home/index.test.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import i18next from 'i18next';
 
-import BookList from '.';
+import Home from './index';
 
 const queryClient = new QueryClient();
 
-describe('BookList component', () => {
-  test('renders BookList', () => {
+describe('Home component', () => {
+  test('matches last snapshot', () => {
     const component = render(
       <QueryClientProvider client={queryClient}>
-        <BookList />
+        <Home />
       </QueryClientProvider>
     );
-    component.getByText(`${i18next.t('Global:loading')}...`);
+    expect(component).toMatchSnapshot();
   });
 });
